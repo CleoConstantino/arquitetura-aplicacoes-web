4004a9debdee9f1fd7316a59a7443240e5f4a3ca
Cléo Constantino <cleoconstantino@outlook.com>
2022-08-19 10:31:31 -0300

    Criamos o servidor express mais simples

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const  http = require('http');     
-const  hostname = '127.0.0.1';
-const  port = 3000;
-
-const  server = http.createServer((_,res) => {
-    res.end('Hello Node');
-});
-
-server.listen(3000, '127.0.0.1');
+const express = require('express');
+    const server = express();
+    
+    server.get('/', (_, res) => {
+	    res.send('Hello Express!');
+    });
+    
+    server.listen(3001);
